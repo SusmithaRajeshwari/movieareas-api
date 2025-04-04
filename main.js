3e3d916c6ed76d8a69ed79c138ee0a06d96aedcb
SusmithaRajeshwari <chandr35@uwindsor.ca>
2025-04-03 21:37:00 -0400

    Routing Structure

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ app.get('/',(req,res)=>{
     res.json({msg:'Hello World'})
 })
 
+//CRUD Functionality
+
+//R- For Reading Movies
+app.get('/movies',()=>{
+
+})
+
+//C- For Creating Movies
+app.post('/movies',()=>{
+
+})
+
+//U- For Updating Movie
+app.put('/movies/:id',()=>{
+
+})
+
+//D - For Deleting Movie
+app.delete('/movies/:id',()=>{
+    
+})
+
 app.listen(PORT,()=>{
     console.log(`App is listening in http://localhost:${PORT}`)
 })
